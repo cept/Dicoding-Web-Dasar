5fbc522dbbd325644ff23b0205b7ca16ed296ebe
cept <49423038+cept@users.noreply.github.com>
2021-01-02 19:44:08 +0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+
+//galeri bisa d klik gambarnya
+
+const box = document.querySelector('.box-image');
+const jumbo = document.querySelector('.jumbo');
+const thumbs = document.querySelectorAll('.thumb');
+
+box.addEventListener('click', function(e){
+	if (e.target.className == 'thumb') {
+		jumbo.src = e.target.src;
+		jumbo.classList.add('fade');
+		setTimeout(function(){
+			jumbo.classList.remove('fade');
+		},500);
+
+		thumbs.forEach(function(thumb){
+			thumb.className = 'thumb';
+		});
+
+		e.target.classList.add('active');
+	}
+});
+
+
+
+
